b69ca70c4f3d4c0b0c5d31fb76f2659221a4a28c
Piash <nazem.piash10@gmail.com>
2022-07-16 06:20:16 +0600

    init vue integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import App from "./layouts/App";
+
+
+
+/**
+ * create a fresh Vue application instance and attach it to the page.
+ */
+const app = new Vue({
+    el: '#app',
+    components: {App},
+});
